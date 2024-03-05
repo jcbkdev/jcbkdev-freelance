dcec50ef591e9c35d51ff0fc7db3a5ccb30107e6
stil-jcbk <=>
2024-03-05 08:41:26 +0100

    contact form session storage

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,36 @@
 const contactDialog = document.getElementById("contact-dialog");
 const body = document.body;
 
+const nameInput = document.getElementById("input-name");
+const emailInput = document.getElementById("input-email");
+const phoneInput = document.getElementById("input-phone");
+const businessInput = document.getElementById("input-business"); //textarea
+const descInput = document.getElementById("input-desc"); //textarea
+
 const openContact = () => {
     if (!contactDialog || contactDialog.open) return;
+
+    let nameStorage = sessionStorage.getItem("input-name");
+    let emailStorage = sessionStorage.getItem("input-email");
+    let phoneStorage = sessionStorage.getItem("input-phone");
+    let businessStorage = sessionStorage.getItem("input-business");
+    let descStorage = sessionStorage.getItem("input-desc");
+
+    if (nameStorage) {
+        nameInput.value = nameStorage;
+    }
+    if (emailStorage) {
+        emailInput.value = emailStorage;
+    }
+    if (phoneStorage) {
+        phoneInput.value = phoneStorage;
+    }
+    if (businessStorage) {
+        businessInput.value = businessStorage;
+    }
+    if (descStorage) {
+        descInput.value = descStorage;
+    }
     contactDialog.show();
     body.classList.add("stop");
 };
@@ -21,3 +49,16 @@ if (contactDialog) {
         }
     });
 }
+
+const setStorage = (e) => {
+    let name = e.target.id;
+    let value = e.target.value;
+
+    sessionStorage.setItem(name, value);
+};
+
+nameInput.addEventListener("input", setStorage);
+emailInput.addEventListener("input", setStorage);
+phoneInput.addEventListener("input", setStorage);
+businessInput.addEventListener("input", setStorage);
+descInput.addEventListener("input", setStorage);
