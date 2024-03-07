aeff39789fa915059a6aa13c8dccef0284b86901
stil-jcbk <=>
2024-03-07 10:30:01 +0100

    sending mail

--- js/contact.js
+++ js/contact.js
@@ -62,3 +62,18 @@ emailInput.addEventListener("input", setStorage);
 phoneInput.addEventListener("input", setStorage);
 businessInput.addEventListener("input", setStorage);
 descInput.addEventListener("input", setStorage);
+
+const contactSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("https://formsubmit.co/jcbkdev@gmail.com", {
+        referrer: "http://localhost:5500/",
+        body: `_captcha=false&name=${nameInput.value}&email=${emailInput.value}&phone-number=${phoneInput.value}&business-description=${businessInput.value}&website-description=${descInput.value}`,
+        method: "POST",
+        mode: "no-cors",
+        credentials: "omit",
+    });
+};
+
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", contactSubmit);
